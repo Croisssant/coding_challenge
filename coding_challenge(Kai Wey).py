726cabd20b4a31c008f8405b42b8dc1c17d52e7e
Kai Wey <limkaiwey@yahoo.com>
2021-07-16 15:47:44 +0100

    Modified coin calculation's if/else to a single loop

--- coding_challenge(Kai Wey).py
+++ coding_challenge(Kai Wey).py
@@ -30,61 +30,37 @@ print("\nTotal in pence: ", input_amount)
 # Allowed coins 200, 100, 50, 20, 10, 5, 2, 1
 # All coins stored in a dictionary
 no_of_coins = {
-    "£2": 0, 
-    "£1": 0,
-    "50p": 0,
-    "20p": 0,
-    "10p": 0,
-    "5p": 0,
-    "2p": 0,
-    "1p": 0,
+    "200": 0, 
+    "100": 0,
+    "50": 0,
+    "20": 0,
+    "10": 0,
+    "5": 0,
+    "2": 0,
+    "1": 0,
 }
 
 # Finding the least amount of coins equivalent to input_amount
 while input_amount != 0:
-    if input_amount >= 200:
-        input_amount -= 200
-        no_of_coins["£2"] += 1
 
-    elif input_amount >= 100:
-        input_amount -= 100
-        no_of_coins["£1"] += 1
-
-    elif input_amount >= 50:
-        input_amount -= 50
-        no_of_coins["50p"] += 1
-
-    elif input_amount >= 20:
-        input_amount -= 20
-        no_of_coins["20p"] += 1
-
-    elif input_amount >= 10:
-        input_amount -= 10
-        no_of_coins["10p"] += 1
-
-    elif input_amount >= 5:
-        input_amount -= 5
-        no_of_coins["5p"] += 1
-
-    elif input_amount >= 2:
-        input_amount -= 2
-        no_of_coins["2p"] += 1
-
-    elif input_amount >= 1:
-        input_amount -= 1
-        no_of_coins["1p"] += 1
-
-
+    for key in no_of_coins:
+ 
+        if input_amount >= int(key):
+            input_amount -= int(key)
+            no_of_coins[key] += 1
+            break
+    
 # Displaying all the amount of coins equivalent to input_amount
 print("\nEquivalent in coins: ")
 for key in no_of_coins:
     if no_of_coins[key] == 0:
         pass
     else:
-        print(key, 'x', no_of_coins[key])
+        if key == "200":
+            print('£2 x ', no_of_coins[key])
 
+        elif key == '100':
+            print('£1 x ', no_of_coins[key])
 
-
-
-
-
+        else:
+            print('{}p x {}'.format(key, no_of_coins[key]))
